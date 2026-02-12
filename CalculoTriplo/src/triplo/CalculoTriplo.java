5d1edaef2e3c17328808559321055bc6a90debd8
MMDDKK6500 <joao.dnwandermuren@senacsp.edu.br>
2026-02-12 20:21:20 -0300

    Adicionar CálculoTriplo

---
+++ CalculoTriplo/src/triplo/CalculoTriplo.java
@@ -0,0 +1,16 @@
+package triplo;
+
+public class CalculoTriplo {
+    
+    int d;
+    
+    
+    public CalculoTriplo(int x) {
+        d = x;
+    }
+    
+    public int Triplo() {
+        return d * 3;
+    }
+    
+}
---
+++ CalculoTriplo/src/triplo/Principal.java
@@ -0,0 +1,21 @@
+package triplo;
+
+import java.util.Scanner;
+
+public class Principal {
+
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+        
+        int x;
+        
+        System.out.println("Informe um número: ");
+        x = sc.nextInt();
+        
+        CalculoTriplo ct = new CalculoTriplo(x);
+    
+        System.out.println("O triplo do número digitado: " + ct.Triplo());
+    }
+    
+}
