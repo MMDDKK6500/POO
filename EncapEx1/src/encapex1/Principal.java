16c0da76fc40783740b3ee6c8dc5948d7c0ac9af
MMDDKK6500 <joao.dnwandermuren@senacsp.edu.br>
2026-02-19 22:33:12 -0300

    Scanner

---
+++ EncapEx1/src/encapex1/Principal.java
@@ -0,0 +1,9 @@
+package encapex1;
+
+public class Principal {
+
+    public static void main(String[] args) {
+        
+    }
+    
+}
--- Encapsulamento1/src/encapsulamento1/Principal.java
+++ Encapsulamento1/src/encapsulamento1/Principal.java
@@ -1,13 +1,20 @@
 package encapsulamento1;
 
+import java.util.Scanner;
+
 public class Principal {
     
     public static void main(String[] args) {
         
+        Scanner sc = new Scanner(System.in);
+        
         Aluno a1 = new Aluno();
         
-        String nome = "Pedrinho";
-        int idade = 21;
+        System.out.println("Qual será o nome?");
+        String nome = sc.nextLine();
+        
+        System.out.println("Qual será a idade?");
+        int idade = sc.nextInt();
         
         a1.setNome(nome);
         a1.setIdade(idade);
