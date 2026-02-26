c6d8bd6bb32b406e75e5beec58d56da668051c81
MMDDKK6500 <joao.dnwandermuren@senacsp.edu.br>
2026-02-26 20:03:50 -0300

    Adicionar Sistema Academico

---
+++ SistemaAcademico/src/boletim/Boletim.java
@@ -0,0 +1,27 @@
+package boletim;
+
+public class Boletim {
+    private float nota;
+    private int frequencia;
+    private String status;
+
+    public Boletim(float n, int f, String s) {
+        this.nota = n;
+        this.frequencia = f;
+        this.status = s;
+    }
+    
+    public Boletim(String st) {
+       this.nota = 10;
+       this.frequencia = 100;
+       this.status = st;
+    }
+    
+    @Override
+    public String toString() {
+        return "Boletim{" + "nota=" + nota + ", frequencia=" + frequencia + ", status=" + status + '}';
+    }
+    
+}
+
+
---
+++ SistemaAcademico/src/boletim/Principal.java
@@ -0,0 +1,31 @@
+package boletim;
+
+import java.util.Scanner;
+
+public class Principal {
+
+    public static void main(String[] args) {
+        
+        float nota;
+        int frequencia;
+        String status;
+        
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.println("Qual foi a nota?");
+        nota = sc.nextFloat();
+        
+        System.out.println("Qual foi a frequencia?");
+        frequencia = sc.nextInt();
+        sc.nextLine();
+        
+        System.out.println("Qual é o status?");
+        status = sc.nextLine();
+        
+        Boletim B1;
+        B1 = new Boletim(nota, frequencia, status);
+        System.out.println(B1.toString());
+        
+    }
+    
+}
